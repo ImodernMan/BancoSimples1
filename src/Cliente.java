dc0694d5e76ae84ce6b350d248f747ad93987a7c
ImodernMan <luizin.sur@gmail.com>
2026-02-17 17:35:05 -0300

    Update Cliente.java
    Implementa classe Cliente com atributos e métodos bancários
    Refatora sistema bancário para orientação a objetos
    Adiciona classe Cliente com depósito e saque
    feat: adiciona classe Cliente com encapsulamento e métodos de depósito/saque

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,4 +1,42 @@
-package PACKAGE_NAME;
-
 public class Cliente {
+
+    private String nome;
+    private String tipoConta;
+    private double saldo;
+
+    public Cliente(String nome, String tipoConta, double saldoInicial) {
+        this.nome = nome;
+        this.tipoConta = tipoConta;
+        this.saldo = saldoInicial;
+    }
+
+    public void depositar(double valor) {
+        if (valor > 0) {
+            saldo += valor;
+            System.out.println("Depósito realizado com sucesso.");
+        } else {
+            System.out.println("Valor inválido.");
+        }
+    }
+
+    public void sacar(double valor) {
+        if (valor <= 0) {
+            System.out.println("Valor inválido.");
+        } else if (valor > saldo) {
+            System.out.println("Saldo insuficiente.");
+        } else {
+            saldo -= valor;
+            System.out.println("Saque realizado com sucesso.");
+        }
+    }
+
+    public void exibirDados() {
+        System.out.println("Nome do Cliente: " + nome);
+        System.out.println("Tipo de Conta: " + tipoConta);
+        System.out.println("Saldo Atual: R$ " + saldo);
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
 }
